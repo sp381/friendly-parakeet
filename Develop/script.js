51300cf7b1ff848fbeb048fce75cae252726f812
sp381 <sarah.powell381@gmail.com>
2022-02-17 11:16:14 -0700

    Added all prompts for special characters and length requirements when clicking the generate button

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,37 @@
 // Assignment code here
+function generatePassword() {
+  var passwordString = ''
+  var length = Number(prompt ("How long would you like your password to be?"))
+  console.log(length)
+  if (length <8 || length >128) {
+    alert ("Must be greater than 8 characters and less than 128.")
+    return
+  }
+  
+  //password character confirmations
+  var lowercase = confirm("Would you like lowercase characters?")
+  console.log(lowercase)
+  var uppercase = confirm("Would you like uppercase characters?")
+  console.log(uppercase)
+  var numeric = confirm("Would you like numeric characters?")
+  console.log(numeric)
+  var uppercase = confirm("Would you like special characters?")
+  
+  if (lowercase === false && uppercase === false) {
+    alert ("Please select at least one option.")
+    return
+  }
 
+  return passwordString
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+  console.log(generateBtn)
 
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +41,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
